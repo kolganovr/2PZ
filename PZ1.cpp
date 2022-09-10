19be0c968acdc90a5d043193e5bba3dbb79e0330
kolganovr <kolganov2003@gmail.com>
2022-09-10 19:02:51 +0300

    Class + adding from keyboard

--- PZ1.cpp
+++ PZ1.cpp
@@ -4,8 +4,48 @@ using namespace std;
 //ПЗ1 второй сем проги
 // https://docs.google.com/document/d/1Yh6SqeuZZLcbPdFe0Zmidrl3CLSAzBG01CuJPle_mt0/edit
 
+class DynArr
+{
+public:
+    DynArr(int size)
+    {
+        FillArrayFromKeyboard(size);
+    }
+
+    void Print()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+private:
+    int *arr;
+    int size = 0;
+
+    void FillArrayFromKeyboard(int length)
+    {
+        cout << "Введите " << length << " элементов массива:" << endl;
+
+        size = length;
+        arr = new int[length];
+        int newVal;
+        for (int i = 0; i < length; i++)
+        {
+            cin >> newVal;
+            arr[i] = newVal;
+        }
+    }
+};
+
 int main()
 {
     setlocale(LC_ALL, "rus"); // Русский язык в консоли
-    cout << "PZ1" << endl;
+
+    int length;
+    cin >> length;
+    DynArr array(length);
+    array.Print();
 }
