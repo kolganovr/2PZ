2c9c5418166497a55148b0603e1b6c8a547bffff
kolganovr <kolganov2003@gmail.com>
2022-09-10 18:45:24 +0300

    initial commit

---
+++ PZ1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//ПЗ1 второй сем проги
+// https://docs.google.com/document/d/1Yh6SqeuZZLcbPdFe0Zmidrl3CLSAzBG01CuJPle_mt0/edit
+
+int main()
+{
+    setlocale(LC_ALL, "rus"); // Русский язык в консоли
+    cout << "PZ1" << endl;
+}
