e9b4076d7d9274f9f3d19713185299bec80d7f1b
kolganovr <kolganov2003@gmail.com>
2022-09-11 19:23:26 +0300

    Coments + destructor

--- PZ1.cpp
+++ PZ1.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 using namespace std;
 
-//ПЗ1 второй сем проги
+// ПЗ1 второй сем проги
 // https://docs.google.com/document/d/1Yh6SqeuZZLcbPdFe0Zmidrl3CLSAzBG01CuJPle_mt0/edit
 
 class DynArr
 {
 public:
+    // конструктор
     DynArr(int size)
     {
         FillArrayFromKeyboard(size);
     }
+    // Деструктор
+    ~DynArr()
+    {
+        delete[] arr;
+    }
 
+    // Выводит весь массив
     void Print()
     {
         for (int i = 0; i < size; i++)
@@ -25,13 +32,15 @@ private:
     int *arr;
     int size = 0;
 
+    /// @brief Создаёт массив и позволяет запонить его с клавиатуры
+    /// @param length Длина массива
     void FillArrayFromKeyboard(int length)
     {
         cout << "Введите " << length << " элементов массива:" << endl;
 
         size = length;
         arr = new int[length];
-        int newVal;
+        int newVal; // Временная переменная для записи ее в массив
         for (int i = 0; i < length; i++)
         {
             cin >> newVal;
