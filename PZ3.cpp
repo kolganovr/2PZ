00a962646dbbe2865b04d3c41ff51392f5558567
kolganovr <kolganov2003@gmail.com>
2022-11-13 18:13:19 +0300

    comments to PZ3

--- PZ3.cpp
+++ PZ3.cpp
@@ -3,30 +3,41 @@
 
 using namespace std;
 
-// generate random strings of small letters and numbers with given length
+/// @brief Generates string with random numbers and letters given length
+/// @param length The length of string
+/// @return The generated string
 string generateRandomString(int length)
 {
+    // Create a string to store the result
     string result = "";
+    // Loop for each character we want to generate
     for (int i = 0; i < length; i++)
     {
+        // Get a random number between 0 and 35
         int random = rand() % 36;
+        // If the number is less than 10, then it is a number
         if (random < 10)
+            // Append the number to the result string
             result += (char)(random + 48);
         else
+            // Otherwise, append the letter to the result string
             result += (char)(random + 87);
     }
+    // Return the result string
     return result;
 }
 
-// generate hash-code of string by Horner's method
-int getHashCodeHorner(string str)
+/// @brief Returns the Hash code of the string by Horner's method
+/// @param str String you need a hash code for
+/// @return Hash code
+int getHashCodeHorner(string str) // Method to return the Horner Hash Code of a given string
 {
-    int result = 0;
-    for (int i = 0; i < str.length(); i++)
+    int hashCode = 0;                      // Initial result is 0
+    for (int i = 0; i < str.length(); i++) // Loop through the string
     {
-        result = result * 37 + (int)str[i];
+        hashCode = hashCode * 37 + (int)str[i]; // Multiply the result by 37 and add the ASCII value of the current letter
     }
-    return result;
+    return hashCode; // Return the final result
 }
 
 int main()
@@ -36,31 +47,33 @@ int main()
     // for lengts from 3 to 6
     for (int length = 3; length <= 6; length++)
     {
-        int collisionsByHash = 0;
-        int collisionsByString = 0;
-        int hashCodes[2000];
-        string strings[2000];
+        int collisionsByHash = 0;   // int to store the number of collisions by hash
+        int collisionsByString = 0; // int to store the number of collisions by string
+        int hashCodes[2000];        // int array to store the hashCodes of the 2000 random strings
+        string strings[2000];       // string array to store the 2000 random strings
 
         // generate 2000 random strings
         for (int i = 0; i < 2000; i++)
         {
-            string str = generateRandomString(length);
-            hashCodes[i] = getHashCodeHorner(str);
-            strings[i] = str;
+            string str = generateRandomString(length); // generate a random string
+            hashCodes[i] = getHashCodeHorner(str);     // get the hashcode of the random string
+            strings[i] = str;                          // store the random string in the array
         }
 
-        clock_t start = clock();
-        for (int i = 0; i < 2000; i++)
+        clock_t start = clock(); // initialize the clock
+
+        for (int i = 0; i < 2000; i++) // iterate through the first 2000 strings
         {
-            for (int j = i + 1; j < 2000; j++)
+            for (int j = i + 1; j < 2000; j++) // iterate through the rest of the strings
             {
-                if (strings[i] == strings[j])
-                    collisionsByString++;
+                if (strings[i] == strings[j]) // compare the strings
+                    collisionsByString++;     // increment the collision counter
             }
         }
-        clock_t end = clock();
-        double timeByString = (double)(end - start) / CLOCKS_PER_SEC;
+        clock_t end = clock();                                        // set the stop time
+        double timeByString = (double)(end - start) / CLOCKS_PER_SEC; // calculate the time elapsed
 
+        // count collisions by hash
         start = clock();
         for (int i = 0; i < 2000; i++)
         {
