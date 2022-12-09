8ec5547c9ff851a2bee19c6a6c87999898488ed9
kolganovr <kolganov2003@gmail.com>
2022-12-09 18:42:13 +0300

    vec func in pz4

--- PZ4.cpp
+++ PZ4.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <string>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -40,8 +41,57 @@ void mapFunc()
     }
 }
 
+void vectorFunc()
+{
+    // random seed
+    srand(time(NULL));
+    // Read the sizes of the two vectors
+    int size1, size2;
+    cin >> size1 >> size2;
+
+    // Create the two vectors and fill them with random numbers between -100 and 100
+    vector<int> vec1(size1), vec2(size2);
+    generate(vec1.begin(), vec1.end(), []() { return rand() % 201 - 100; });
+    generate(vec2.begin(), vec2.end(), []() { return rand() % 201 - 100; });
+
+    // Output the contents of the two vectors
+    cout << "Vector 1:";
+    for (const auto &x : vec1)
+        cout << ' ' << x;
+    cout << '\n';
+
+    cout << "Vector 2:";
+    for (const auto &x : vec2)
+        cout << ' ' << x;
+    cout << '\n';
+
+    // Find the arithmetic mean of the elements of each vector
+    double mean1 = 0, mean2 = 0;
+    for (const auto &x : vec1)
+        mean1 += x;
+    mean1 /= vec1.size();
+
+    for (const auto &x : vec2)
+        mean2 += x;
+    mean2 /= vec2.size();
+
+    cout << "Mean of vector 1: " << mean1 << '\n';
+    cout << "Mean of vector 2: " << mean2 << '\n';
+
+    // Find the intersecting elements of the two vectors
+    vector<int> intersect;
+    set_intersection(vec1.begin(), vec1.end(), vec2.begin(), vec2.end(),
+                          back_inserter(intersect));
+
+    cout << "Intersecting elements:";
+    for (const auto &x : intersect)
+        cout << ' ' << x;
+    cout << '\n';
+}
+
 int main()
 {
-    mapFunc();
+    // mapFunc();
+    vectorFunc();
     return 0;
 }
